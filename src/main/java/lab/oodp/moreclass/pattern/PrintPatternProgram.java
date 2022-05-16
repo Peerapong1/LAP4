d9916b0e99b6e79aee19558db0ed3faac288f883
Windows 10 ASUS <Windows 10 ASUS@LAPTOP-BL9AOFBP>
2022-05-16 14:12:49 +0700

    Lab

--- src/main/java/lab/oodp/moreclass/pattern/PrintPatternProgram.java
+++ src/main/java/lab/oodp/moreclass/pattern/PrintPatternProgram.java
@@ -11,29 +11,39 @@ public class PrintPatternProgram {
     public void printPatternOne() {
         System.out.println("First Pattern");
         //TODO uncomment the lines below
-//        Pattern top = new Pattern(15, '*');
-//        
-//        Pattern sideOfFirstLine = new Pattern(7, '#');
-//        Pattern sideOfLine = new Pattern(7, '~');
-//        Pattern middle = new Pattern(1, '.');
-//        
-//        System.out.println(top);
-//        System.out.println(sideOfFirstLine.toString() + middle.toString() + sideOfFirstLine.toString());
-//        
-//        for (int i = 0; i < 6; i++) {
-//            middle.setNumberOfCharacters(middle.getNumberOfCharacters() + 1);
-//            System.out.println(sideOfLine.toString() + middle.toString() + sideOfLine.toString());
-//        }
+        Pattern top = new Pattern(15, '*');
+        
+        Pattern sideOfFirstLine = new Pattern(7, '#');
+        Pattern sideOfLine = new Pattern(7, '~');
+        Pattern middle = new Pattern(1, '.');
+
+       System.out.println(top);
+       System.out.println(sideOfFirstLine.toString() + middle.toString() + sideOfFirstLine.toString());
+        
+        for (int i = 0; i < 6; i++) {
+           middle.setNumberOfCharacters(middle.getNumberOfCharacters() + 1);
+            System.out.println(sideOfLine.toString() + middle.toString() + sideOfLine.toString());
+        }
     }
     
     public void printPatternTwo() {
         System.out.println("Second Pattern");
         
         //TODO: create pattern class and print outs first two line
+        Pattern top = new Pattern(36, '@');
+//      
+       Pattern sideOfFirstLine = new Pattern(12, '=');
+       Pattern sideOfLine = new Pattern(12, '&');
+       Pattern middle = new Pattern(12, '.');
+//      
+      System.out.println(top);
+     System.out.println(sideOfFirstLine.toString() + middle.toString() + sideOfFirstLine.toString());
  
         for (int i = 0; i < 6; i++) {
         	//TODO: print out the lines with & and .
-        	
+        	sideOfLine.setNumberOfCharacters(sideOfLine.getNumberOfCharacters() + 1);
+        	middle.setNumberOfCharacters(middle.getNumberOfCharacters() - 2);
+            System.out.println(sideOfLine.toString() + middle.toString() + sideOfLine.toString());
         }
 
 
